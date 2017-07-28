9d71ff2d6f7d0dfaa6bd692aa5c84f578cb7b863
Quentin Bergougnoux <quentin.bergougnoux@univ-lille1.fr>
2017-07-28 14:34:54 +0200

    Added missing callgate helpers to x86 lipip

--- include/pip/api.h
+++ include/pip/api.h
@@ -7,10 +7,15 @@ uint32_t createPartition(uint32_t root, uint32_t pd, uint32_t sh1, uint32_t sh2,
 uint32_t pageMapCount(uint32_t root, uint32_t target);
 uint32_t prepare(uint32_t root, uint32_t target, uint32_t list, uint32_t add_sh3);
 uint32_t mapPage(uint32_t src, uint32_t root, uint32_t target, uint32_t r, uint32_t w, uint32_t x);
+uint32_t unmapPage(uint32_t part, uint32_t vaddr);
 uint32_t notify2(uint32_t part, uint32_t int_no, uint32_t data1, uint32_t data2);
 #define notify(part,no) notify2(part,no,0,0)
 uint32_t dispatch(uint32_t part, uint32_t int_no, uint32_t data1, uint32_t data2);
 uint32_t pip_time(void);
 uint32_t resume(uint32_t desc, uint32_t ctx);
 uint32_t viret(void);
+uint32_t mappedInChild(uint32_t vad);
+uint32_t deletePartition(uint32_t desc);
+uint32_t collect(uint32_t part, uint32_t target);
+
 #endif
