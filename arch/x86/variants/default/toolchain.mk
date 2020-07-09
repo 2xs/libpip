89e2f3f57c495e18f6e019694844d9f902c8d400
Florian Vanhems <florian.vanhems@univ-lille.fr>
2020-07-09 12:18:30 +0200

    Add missing compiler options (-fno-pic, -no-pie)

--- arch/x86/variants/default/toolchain.mk
+++ arch/x86/variants/default/toolchain.mk
@@ -6,11 +6,11 @@ AS=i386-elf-gcc
 AR=i386-elf-ar
 
 # C flags
-CFLAGS=-c -ffreestanding -nostdlib -Wall -Werror -Wextra -fno-builtin -Wno-unused-parameter -Wno-unused-variable -m32 -O2 -fno-caller-saves
+CFLAGS=-c -ffreestanding -nostdlib -Wall -Werror -Wextra -fno-builtin -Wno-unused-parameter -Wno-unused-variable -m32 -fno-caller-saves -fno-stack-protector -fno-pic -no-pie -O2
 CFLAGS+=$(foreach dir, $(INCDIR), -I$(dir))
 
 # Assembler flags
-ASFLAGS=-m32 -c -I. -I./include -fomit-frame-pointer --freestanding -nostdlib -fno-stack-protector
+ASFLAGS=-m32 -c -I. -I./include -fomit-frame-pointer --freestanding -nostdlib -fno-stack-protector -fno-pic -no-pie
 
 # Archiver flags
 ARFLAGS=rcs
