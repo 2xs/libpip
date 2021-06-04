702c8b30cf885ce46c106cda5147ddd3326ea873
Damien Amara <>
2021-06-04 15:18:05 +0200

    Remove trailing whitespace

--- arch/x86/variants/galileo/toolchain.mk
+++ arch/x86/variants/galileo/toolchain.mk
@@ -6,7 +6,7 @@ AS=nasm
 AR=ar
 
 # C flags
-CFLAGS=-c -ffreestanding -nostdlib -Wall -Werror -Wextra -fno-builtin -Wno-unused-parameter -Wno-unused-variable -m32 -O2 -fno-caller-saves -fno-stack-protector -fno-pic -no-pie 
+CFLAGS=-c -ffreestanding -nostdlib -Wall -Werror -Wextra -fno-builtin -Wno-unused-parameter -Wno-unused-variable -m32 -O2 -fno-caller-saves -fno-stack-protector -fno-pic -no-pie
 CFLAGS+=$(foreach dir, $(INCDIR), -I$(dir))
 
 # Assembler flags
